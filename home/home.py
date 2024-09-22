c6f13e46163124a67ac0e9a83bd82c935bca0fac
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-09-22 18:01:38 +1200

    Add sorting function to the Pokemon index table

--- home/home.py
+++ home/home.py
@@ -10,3 +10,23 @@ def home():
     return render_template('index.html')
 
 
+@home_blueprint.route('/pokemons', methods=['GET'])
+def browse_pokemons():
+    page = int(request.args.get('page', 1))
+    sort = request.args.get('sort', 'id')  # Default sort by ID
+    order = request.args.get('order', 'asc')  # Default order ascending
+
+    pokemon_data, total_pages = fetch_pokemon_data(page)
+
+    # Sort the data based on the query parameters
+    if sort in ['height', 'weight']:
+        pokemon_data = sorted(pokemon_data, key=lambda x: x[sort], reverse=(order == 'desc'))
+    elif sort == 'type':
+        pokemon_data = sorted(pokemon_data, key=lambda x: x['types'][0] if x['types'] else '', reverse=(order == 'desc'))
+    elif sort == 'id':
+        pokemon_data = sorted(pokemon_data, key=lambda x: x['id'], reverse=(order == 'desc'))
+    elif sort == 'name':
+        pokemon_data = sorted(pokemon_data, key=lambda x: x['name'].lower(), reverse=(order == 'desc'))
+
+    return render_template('pokemonList.html', pokemon_data=pokemon_data, total_pages=total_pages,
+                           page=page, sort=sort, order=order)
