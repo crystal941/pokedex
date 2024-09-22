55ee51c9fc29588e4fc9ab900af50eec75b948e8
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-09-22 18:16:57 +1200

    Register blueprints

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
+from flask import Flask
 
 
-@app.route('/')
 def create_app():
-    return render_template('index.html')
+    flask_app = Flask(__name__)
+
+    with flask_app.app_context():
+        # Register blueprints
+        from home import home as home_blueprint
+        flask_app.register_blueprint(home_blueprint.home_blueprint)
+
+        from browse import browse as browse_blueprint
+        flask_app.register_blueprint(browse_blueprint.browse_blueprint)
+
+    return flask_app
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, threaded=False)
+    try:
+        app = create_app()
+        app.run(host='localhost', port=5000, threaded=False)
+    except Exception as e:
+        print(f"An error occurred: {e}")
